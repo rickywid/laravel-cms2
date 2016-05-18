041374763371eb3a085a49b466990cc0a0e3b1f0
Ricky JW <rickywid@hotmail.com>
2016-05-18 14:47:58 -0400

    edit config/app.php file

--- config/app.php
+++ config/app.php
@@ -91,7 +91,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomStringSomeRandomString'),
+    'key' => env('SomeRandomStringSomeRandomString'),
 
     'cipher' => 'AES-256-CBC',
 
